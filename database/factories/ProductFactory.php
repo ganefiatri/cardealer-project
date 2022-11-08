af12c52919fe638a03a7fc18a2360cad4366d036
ganefiatri <gicerf@gmail.com>
2022-11-08 07:05:15 +0700

    add pagination, search, sorting

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,25 @@
+<?php
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(30),
+            'description' => $this->faker->text(),
+            'price' => $this->faker->numberBetween(10, 200),
+        ];
+    }
+}
